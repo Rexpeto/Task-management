a2206b244a21db663c5dee465c10d6054d5a297d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-06 16:35:33 -0400

    Model User

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            trim: true,
+            unique: true,
+        },
+        toke: {
+            type: String,
+        },
+        confirm: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
