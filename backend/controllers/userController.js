1cf79101ffec53c4f5da79157b2e3ac70067d774
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-06 22:16:01 -0400

    Register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,20 @@
-export const userExample = (req, res) => {
-    res.send("Hi user");
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    const { email } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+        const error = new Error("El usuario ya existe");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+        const user = new User(req.body);
+        const userDb = await user.save();
+        res.send(userDb);
+    } catch (error) {
+        console.log(error);
+    }
 };
