a7b88ca9bcbb16f745b462137e0ede16d3275e4c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-07 12:34:14 -0400

    auth account

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,3 +20,32 @@ export const register = async (req, res) => {
         console.log(error);
     }
 };
+
+export const authAccount = async (req, res) => {
+    const { email, password } = req.body;
+
+    //? check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error("El usuario no existe");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    //? check if user is confirmed
+    if (!user.confirm) {
+        const error = new Error("Tu cuenta no ha sido confirmada");
+        return res.status(403).json({ msg: error.message });
+    }
+
+    //? check password
+    if (await user.verifyPassword(password)) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        const error = new Error("Contraseña incorrecta");
+        return res.status(403).json({ msg: error.message });
+    }
+};
