2a5c17d698a8a56bdaf2ee47e4b96a4d3b7d5a3d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-06 22:15:46 -0400

    Use register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { userExample } from "../controllers/userController.js";
+import { register } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/", userExample);
+router.post("/", register);
 
 export default router;
