a7cf5d6e47922478b1fca8c0219846ba8b08c988
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 15:42:18 -0400

    Auth from jwt

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -1,5 +1,29 @@
-const checkAuth = (req, res, next) => {
-    console.log("Initial middleware");
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const checkAuth = async (req, res, next) => {
+    const { authorization } = req.headers;
+    let token;
+
+    if (authorization && authorization.startsWith("Bearer")) {
+        try {
+            token = authorization.split(" ")[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select(
+                "-password -confirm -token -createdAt -updatedAt -__v"
+            );
+
+            return next();
+        } catch (error) {
+            return res.status(404).json({ msg: "Oops!! ocurrio un error" });
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({ msg: "No autenticado" });
+    }
 };
 
 export default checkAuth;
