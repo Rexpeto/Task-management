c3b612149d728b764ea6f36a1e4a76822f1391b8
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 17:10:29 -0400

    Model from project

---
+++ backend/models/Project.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const projectSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        deadline: {
+            type: Date,
+            default: Date.now(),
+        },
+        clients: {
+            type: String,
+            trim: true,
+            require: true,
+        },
+        creator: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        collaborator: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
