162ddba655a804aea3d04d716394aa2c30a18274
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 11:59:46 -0400

    Route confirm email

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { register, authAccount } from "../controllers/userController.js";
+import {
+    register,
+    authAccount,
+    confirmEmail,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", register);
 router.post("/login", authAccount);
+router.get("/confirm/:token", confirmEmail);
 
 export default router;
