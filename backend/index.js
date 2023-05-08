cfe1f341a46e00565f50852fdd5936272fdb8fad
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 17:36:09 -0400

    Use project

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connect from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ connect();
 
 //? Routing
 app.use("/api/user", userRoutes);
+app.use("/api/project", projectRoutes);
 
 const port = process.env.PORT || 4000;
 
