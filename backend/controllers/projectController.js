e37a4b15dc726d4d7ccf902bcc7df3e00d20dc44
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 17:23:07 -0400

    Controllers the projects

---
+++ backend/controllers/projectController.js
@@ -0,0 +1,25 @@
+import Project from "../models/Project";
+
+//? Get project the users
+export const getProject = async (req, res) => {};
+
+//? Get projects the users
+export const getProjects = async (req, res) => {};
+
+//? New project for user
+export const newProject = async (req, res) => {};
+
+//? Edit project for user
+export const editProject = async (req, res) => {};
+
+//? Delete project
+export const deleteProject = async (req, res) => {};
+
+//? Add collaborators
+export const addCollaborators = async (req, res) => {};
+
+//? Delete collaborators
+export const deleteCollaborators = async (req, res) => {};
+
+//? Get task
+export const getTask = async (req, res) => {};
