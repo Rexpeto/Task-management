65ffee8b936eeeed93810aaaf399deefa2a36249
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-08 13:01:18 -0400

    Route from forget password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
     register,
     authAccount,
     confirmEmail,
+    forgotPassword,
 } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/", register);
 router.post("/login", authAccount);
 router.get("/confirm/:token", confirmEmail);
+router.post("/forgotPass", forgotPassword);
 
 export default router;
