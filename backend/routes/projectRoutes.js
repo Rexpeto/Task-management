336f92369829b34711a329d1d2261f5a6a8ba378
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-09 10:33:19 -0400

    Routes finish

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,9 +1,30 @@
 import express from "express";
 import checkAuth from "../middleware/checkAuth.js";
-import { getProject, getProjects } from "../controllers/projectController.js";
+import {
+    addCollaborators,
+    deleteCollaborators,
+    deleteProject,
+    editProject,
+    getProject,
+    getProjects,
+    getTask,
+    newProject,
+} from "../controllers/projectController.js";
 
 const router = express.Router();
 
-router.get("/", checkAuth, getProjects);
+router.route("/").get(checkAuth, getProjects).post(checkAuth, newProject);
+
+router
+    .route("/:id")
+    .get(checkAuth, getProject)
+    .put(checkAuth, editProject)
+    .delete(checkAuth, deleteProject);
+
+router.get("/task/:id", checkAuth, getTask);
+
+router.post("/add-collaborators/:id", checkAuth, addCollaborators);
+
+router.post("/delete-collaborators/:id", checkAuth, deleteCollaborators);
 
 export default router;
