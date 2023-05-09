cafc49deb9bc26c950d94b103ae62d9b9247137e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-09 11:05:32 -0400

    Get project from id

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,7 +1,25 @@
 import Project from "../models/Project.js";
 
 //? Get project the users
-export const getProject = async (req, res) => {};
+export const getProject = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const project = await Project.findById(id);
+
+        if (!project) {
+            return res.status(404).json({ msg: "El proyecto no existe" });
+        }
+
+        if (project.creator.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ msg: "Acción no válida" });
+        }
+
+        res.status(200).json(project);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 //? Get projects the users
 export const getProjects = async (req, res) => {
