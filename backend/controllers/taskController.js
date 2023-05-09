732a2fa8147321dc1f5849fdb8bcf108b3efc0f3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-09 12:53:20 -0400

    Controllers task

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,11 @@
+import Task from "../models/Task.js";
+
+export const addTask = async (req, res) => {};
+
+export const getTask = async (req, res) => {};
+
+export const updateTask = async (req, res) => {};
+
+export const deleteTask = async (req, res) => {};
+
+export const changeStatus = async (req, res) => {};
