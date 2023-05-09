cdd8cd5ba63fd0e1918b4dd6a4907ee31ef196f7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-09 11:23:22 -0400

    Edit project

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -45,7 +45,32 @@ export const newProject = async (req, res) => {
 };
 
 //? Edit project for user
-export const editProject = async (req, res) => {};
+export const editProject = async (req, res) => {
+    const { id } = req.params;
+    const { name, description, clients, deadline } = req.body;
+
+    try {
+        const project = await Project.findById(id);
+
+        if (!project) {
+            return res.status(404).json({ msg: "El proyecto no existe" });
+        }
+
+        if (project.creator.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ msg: "Acción no válida" });
+        }
+
+        project.name = name || project.name;
+        project.description = description || project.description;
+        project.clients = clients || project.clients;
+        project.deadline = deadline || project.deadline;
+
+        const projectStore = await project.save();
+        res.status(200).json(projectStore);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 //? Delete project
 export const deleteProject = async (req, res) => {};
