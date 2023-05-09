dd9197fbf50b8ec97dd5faf0742861b8f22f4e94
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-09 12:52:38 -0400

    Endpoints task

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import checkAuth from "../middleware/checkAuth.js";
+import {
+    addTask,
+    changeStatus,
+    deleteTask,
+    getTask,
+    updateTask,
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router.post("/", checkAuth, addTask);
+router
+    .route("/:id")
+    .get(checkAuth, getTask)
+    .put(checkAuth, updateTask)
+    .delete(checkAuth, deleteTask);
+router.post("/status/:id", checkAuth, changeStatus);
+
+export default router;
