a6704396c0655f0032ef19bf8dd310d309f93df2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-10 12:19:45 -0400

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
-import Login from "./pages/Login";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import NewPassword from "./pages/auth/NewPassword";
+import ConfirmPass from "./pages/auth/ConfirmPass";
 
 const App = () => {
     return (
@@ -8,6 +12,10 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<AuthLayout />}>
                     <Route index element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="forgotPass" element={<ForgotPassword />} />
+                    <Route path="forgotPass/:token" element={<NewPassword />} />
+                    <Route path="confirm/:id" element={<ConfirmPass />} />
                 </Route>
                 <Route path="/admin"></Route>
             </Routes>
