9b9260fd8a73129205e45b1d02dacfc9ea11dce4
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-10 11:42:06 -0400

    Update

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <div className="max-w-sm w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <form>
+                <div className="mb-6">
+                    <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Correo
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 transition duration-150 outline-none"
+                        placeholder="nombre@dominio.com"
+                        required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 transition duration-150 outline-none"
+                        required
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
+                >
+                    Iniciar
+                </button>
+                <div className="flex flex-row gap-3 justify-center mt-6">
+                    <p>¿No tienes una cuenta?</p>
+                    <Link
+                        className="hover:text-blue-600 transition duration-150"
+                        to="register"
+                    >
+                        Registrarse
+                    </Link>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
