631c212b197a2f69bbf335eead6223fa1a526f77
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-10 14:59:42 -0400

    Use cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connect from "./config/db.js";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
@@ -10,6 +11,20 @@ app.use(express.json());
 dotenv.config();
 
 connect();
+
+const whiteList = ["http://192.168.1.105:5173/"];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Error de Cors"));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 
 //? Routing
 app.use("/api/user", userRoutes);
