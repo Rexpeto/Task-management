a835641a16368fbd966c3e0971ec70c4a87bb749
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-11 11:39:48 -0400

    Finish forgot password

--- frontend/src/pages/auth/ForgotPassword.jsx
+++ frontend/src/pages/auth/ForgotPassword.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const ForgotPassword = () => {
+    const [email, setEmail] = useState("");
+
+    const handlerSubmit = async (e) => {
+        e.preventDefault();
+
+        if (email === "") {
+            toast.warn("Debe colocar el correo de su cuenta");
+            return;
+        }
+
+        try {
+            const { data } = await axios.post(
+                `${import.meta.env.VITE_BACKEND_URL}/user/forgotPass`,
+                { email: email.toLocaleLowerCase() }
+            );
+
+            setEmail("");
+            toast.success(data.msg);
+        } catch ({ response: { data } }) {
+            toast.error(data.msg);
+        }
+    };
+
     return (
         <div className="max-w-sm w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <h3 className="text-lg font-bold mb-6">Recuperar contraseña</h3>
-            <form>
+            <form onSubmit={handlerSubmit}>
                 <div className="mb-6">
                     <label
                         htmlFor="email"
@@ -16,8 +42,9 @@ const ForgotPassword = () => {
                         type="email"
                         id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 transition duration-150 outline-none"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
                         placeholder="nombre@dominio.com"
-                        required
                     />
                 </div>
                 <button
