54017c1df3e31500a3d1be447d67c53319b50681
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-15 15:57:26 -0400

    Create new project

--- frontend/src/context/ProjectProvider.jsx
+++ frontend/src/context/ProjectProvider.jsx
@@ -1,11 +1,37 @@
 import { createContext, useState, useEffect } from "react";
 import clientAxios from "../config/clientAxios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const ProjectContext = createContext();
 
 export const ProjectProvider = ({ children }) => {
-    const submitProject = (project) => {
-        console.log(project);
+    const navigate = useNavigate();
+
+    const submitProject = async (project) => {
+        const token = localStorage.getItem("access_token");
+
+        if (!token) return;
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        try {
+            const { data } = await clientAxios.post(
+                "/project",
+                project,
+                config
+            );
+
+            toast.success("Proyecto creado correctamente");
+            navigate("/project");
+        } catch ({ response }) {
+            toast.warn(response.data.msg);
+        }
     };
 
     return (
