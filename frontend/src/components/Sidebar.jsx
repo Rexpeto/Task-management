6212d5ac4f3ba4e9f3fc5bea51c575534d15d0c5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-14 23:40:46 -0400

    Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import { SiWheniwork } from "react-icons/si";
+
 const Sidebar = () => {
     return (
-        <div>
-            <h1>Sidebar</h1>
-        </div>
+        <aside className="w-[13.5rem] shadow bg-gray-800/50">
+            <nav className="py-4 px-2">
+                <Link
+                    href="#"
+                    className="flex items-center gap-3 py-2 px-2 rounded bg-gray-700/30 hover:bg-gray-700 transition duration-150"
+                >
+                    <SiWheniwork className="text-lg" />
+                    Crear Proyecto
+                </Link>
+            </nav>
+        </aside>
     );
 };
 
