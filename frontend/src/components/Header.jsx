60621b4d0d32b4519210a8486eaadb250211c048
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-14 23:40:27 -0400

    Update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     const [dropdown, setDropdown] = useState(false);
 
     return (
-        <header className="px-4 py-3 bg-gray-800 shadow fixed w-full">
+        <header className="px-4 py-3 bg-gray-800/50 shadow fixed w-full">
             <div className="md:flex md:justify-between">
                 <div className="flex gap-2 items-center">
                     <img src="favicon.svg" alt="logo" />
@@ -13,16 +13,16 @@ const Header = () => {
                 </div>
 
                 <form className="min-w-[50rem]">
-                    <div class="flex">
-                        <div class="relative w-full">
+                    <div className="flex">
+                        <div className="relative w-full">
                             <input
                                 type="search"
-                                class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 rounded-l-lg transition outline-none duration-150"
+                                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 rounded-l-lg transition outline-none duration-150"
                                 placeholder="Buscar"
                             />
                             <button
                                 type="submit"
-                                class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 outline-none dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
+                                className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 outline-none dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
                             >
                                 <RiSearchLine className="text-xl" />
                             </button>
@@ -37,7 +37,7 @@ const Header = () => {
                     >
                         <img
                             type="button"
-                            class="w-8 h-8 rounded"
+                            className="w-8 h-8 rounded"
                             src="profile.jpg"
                             alt="User"
                         />
@@ -50,20 +50,20 @@ const Header = () => {
                     </div>
 
                     <div
-                        class={`z-10 ${
+                        className={`z-10 ${
                             dropdown ? "visible" : "hidden"
-                        } top-[3.5rem] right-2 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 transition-all duration-150 overflow-hidden`}
+                        } top-[3.5rem] right-2 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-800/80 dark:divide-gray-600 transition-all duration-150 overflow-hidden`}
                     >
-                        <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                        <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
                             <div>Carlos Gallardo</div>
-                            <div class="font-medium truncate">
+                            <div className="font-medium truncate">
                                 carlos@correo.com
                             </div>
                         </div>
-                        <div class="py-1">
+                        <div className="py-1">
                             <a
                                 href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white transition duration-150"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white transition duration-150"
                             >
                                 Cerrar sesión
                             </a>
