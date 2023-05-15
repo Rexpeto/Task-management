0822f5c494d1a95864f79eda5c6997ebad56112e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-15 17:10:02 -0400

    Show projects

--- frontend/src/pages/admin/Project.jsx
+++ frontend/src/pages/admin/Project.jsx
@@ -1,8 +1,26 @@
+import CardProject from "../../components/CardProject";
+import useProject from "../../hook/useProject";
+
 const Project = () => {
+    const { projects } = useProject();
+
     return (
-        <div>
-            <h1>Project</h1>
-        </div>
+        <>
+            <h1 className="text-2xl font-bold">Proyectos</h1>
+
+            {projects.length ? (
+                <div className="grid grid-cols-3 mt-5 gap-4">
+                    {projects.map((project, index) => (
+                        <CardProject project={project} key={index} />
+                    ))}
+                </div>
+            ) : (
+                <div className="flex flex-col gap-2 items-center mt-5">
+                    <h2 className="text-xl font-semibold">No hay proyectos</h2>
+                    <p className="text-lg font-semibold">Agregue uno</p>
+                </div>
+            )}
+        </>
     );
 };
 
