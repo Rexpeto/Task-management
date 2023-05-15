c25d38a825d002482b22d229390cfa951ce74eec
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-14 23:08:39 -0400

    Layout dashboard

--- frontend/src/layouts/RoutePrivate.jsx
+++ frontend/src/layouts/RoutePrivate.jsx
@@ -1,5 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hook/useAuth";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const RoutePrivate = () => {
     const { auth, loading } = useAuth();
@@ -8,7 +10,23 @@ const RoutePrivate = () => {
         return;
     }
 
-    return <>{auth._id ? <Outlet /> : <Navigate to="/" />}</>;
+    return (
+        <>
+            {auth._id ? (
+                <div>
+                    <Header />
+                    <div className="md:flex md:min-h-screen pt-[4rem]">
+                        <Sidebar />
+                        <main>
+                            <Outlet />
+                        </main>
+                    </div>
+                </div>
+            ) : (
+                <Navigate to="/" />
+            )}
+        </>
+    );
 };
 
 export default RoutePrivate;
