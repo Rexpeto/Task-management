0bd45ea0467a8c4c8eefd933168dc4670983dbee
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-15 15:16:54 -0400

    Use form project

--- frontend/src/pages/admin/NewProject.jsx
+++ frontend/src/pages/admin/NewProject.jsx
@@ -1,8 +1,13 @@
+import FormProject from "../../components/FormProject";
+
 const NewProject = () => {
     return (
-        <div>
-            <h1>New Project</h1>
-        </div>
+        <>
+            <h1 className="text-2xl font-bold">Crear proyecto</h1>
+            <div className="flex mt-10">
+                <FormProject />
+            </div>
+        </>
     );
 };
 
