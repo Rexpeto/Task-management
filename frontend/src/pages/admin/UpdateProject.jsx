6fb14b340d62f985183bc1c6613e5e112b5d1055
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-18 00:26:10 -0400

    Button delete

--- frontend/src/pages/admin/UpdateProject.jsx
+++ frontend/src/pages/admin/UpdateProject.jsx
@@ -1,16 +1,23 @@
 import { useEffect } from "react";
 import useProject from "../../hook/useProject";
-import { useParams } from "react-router-dom";
 import FormProject from "../../components/FormProject";
+import { useParams } from "react-router-dom";
+import { MdDeleteSweep } from "react-icons/md";
 
 const UpdateProject = () => {
     const { id } = useParams();
-    const { project, getProject, loading } = useProject();
+    const { project, getProject, loading, deleteProject } = useProject();
     const { name } = project;
 
     useEffect(() => {
         getProject(id);
     }, []);
+
+    const handleClick = async () => {
+        if (confirm("¿Desea eliminar este proyecto?")) {
+            await deleteProject(id);
+        }
+    };
 
     return loading ? (
         <div className="w-full h-full flex items-center justify-center">
@@ -18,7 +25,16 @@ const UpdateProject = () => {
         </div>
     ) : (
         <div>
-            <h1 className="text-2xl font-bold">{`Editar proyecto ${name}`}</h1>
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-bold">{`Editar proyecto ${name}`}</h1>
+                <button
+                    className="flex items-center gap-2  hover:text-red-600 transition duration-150"
+                    onClick={handleClick}
+                >
+                    Eliminar
+                    <MdDeleteSweep className="text-2xl" />
+                </button>
+            </div>
             <div className="flex mt-10">
                 <FormProject />
             </div>
