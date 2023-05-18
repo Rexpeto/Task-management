70732b73e2cfe8f52fcf6ff9bce0dd9fd3c9f2b4
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-17 22:15:53 -0400

    Show projects to user

--- frontend/src/pages/admin/Project.jsx
+++ frontend/src/pages/admin/Project.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useProject from "../../hook/useProject";
+
 const Project = () => {
-    return (
+    const { id } = useParams();
+    const { getProject, project, loading } = useProject();
+
+    useEffect(() => {
+        getProject(id);
+    }, []);
+
+    return loading ? (
+        <div className="w-full h-full flex items-center justify-center">
+            <span class="loader"></span>
+        </div>
+    ) : (
         <div>
-            <h1>Proyecto</h1>
+            <h1 className="first-letter:uppercase font-bold text-2xl">
+                {project?.name}
+            </h1>
         </div>
     );
 };
