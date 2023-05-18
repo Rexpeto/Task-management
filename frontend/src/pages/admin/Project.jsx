fbdf9d1717d3463d3a27e187ace40edbabc44a52
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-17 22:44:28 -0400

    Button edit project

--- frontend/src/pages/admin/Project.jsx
+++ frontend/src/pages/admin/Project.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { MdEditNote } from "react-icons/md";
 import useProject from "../../hook/useProject";
 
 const Project = () => {
@@ -10,15 +11,29 @@ const Project = () => {
         getProject(id);
     }, []);
 
+    const { name, description } = project;
+
     return loading ? (
         <div className="w-full h-full flex items-center justify-center">
-            <span class="loader"></span>
+            <span className="loader"></span>
         </div>
     ) : (
-        <div>
-            <h1 className="first-letter:uppercase font-bold text-2xl">
-                {project?.name}
-            </h1>
+        <div className="flex justify-between items-center">
+            <div>
+                <h1 className="first-letter:uppercase font-bold text-2xl">
+                    {name}
+                </h1>
+                <p className=" text-gray-300 first-letter:uppercase">
+                    {description}
+                </p>
+            </div>
+            <Link
+                className="flex items-center gap-2 text-gray-400 hover:text-white transition duration-150"
+                to={`/projects/edit/${id}`}
+            >
+                <p>Editar</p>
+                <MdEditNote className="text-3xl" />
+            </Link>
         </div>
     );
 };
