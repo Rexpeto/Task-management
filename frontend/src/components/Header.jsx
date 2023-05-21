4fbde1abf0a01c2087572bddeb06b845ca789fbe
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-21 16:46:56 -0400

    Dropdown user header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import useAuth from "../hook/useAuth";
 import { RiSearchLine, RiArrowDropDownLine } from "react-icons/ri";
 
-const Header = () => {
-    const [dropdown, setDropdown] = useState(false);
+const Header = ({setDropdown, dropdown}) => {
+    
     const { auth } = useAuth();
 
     return (
--- frontend/src/layouts/RoutePrivate.jsx
+++ frontend/src/layouts/RoutePrivate.jsx
@@ -3,9 +3,11 @@ import useAuth from "../hook/useAuth";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { ToastContainer } from "react-toastify";
+import { useState } from "react";
 
 const RoutePrivate = () => {
     const { auth, loading } = useAuth();
+    const [dropdown, setDropdown] = useState(false);
 
     if (loading) {
         return;
@@ -16,8 +18,8 @@ const RoutePrivate = () => {
             {auth._id ? (
                 <div>
                     <ToastContainer theme="dark" />
-                    <Header />
-                    <div className="md:flex md:min-h-screen pt-[4rem]">
+                    <Header setDropdown={setDropdown} dropdown={dropdown} />
+                    <div className="md:flex md:min-h-screen pt-[4rem]" onClick={() => setDropdown(false)}>
                         <Sidebar />
                         <main className="p-4 pl-[13rem] w-full">
                             <Outlet />
