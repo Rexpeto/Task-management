8d1e3d58cb9c0b1bdd20819becb43ff83039af18
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-24 20:39:48 -0400

    Fix button

--- frontend/src/components/CardCollaborator.jsx
+++ frontend/src/components/CardCollaborator.jsx
@@ -12,7 +12,7 @@ const CardCollaborator = ({ user }) => {
 
     return (
         <div className="md:max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex flex-col items-center p-3 md:py-10  md:gap-0">
+            <div className="md:flex md:flex-col md:items-center p-3 md:py-10  md:gap-0">
                 <div className="flex flex-row md:flex-col items-center gap-4 md:gap-0">
                     <img
                         className="w-24 h-24 mb-3 rounded-full shadow-lg"
@@ -31,7 +31,7 @@ const CardCollaborator = ({ user }) => {
                         {collaborator[0]?._id === _id ? (
                             <button
                                 type="button"
-                                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 outline-none dark:bg-red-600 dark:hover:bg-red-700 transition duration-150"
+                                className="block w-full md:inline-flex md:items-center md:w-initial px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 outline-none dark:bg-red-600 dark:hover:bg-red-700 transition duration-150"
                                 onClick={() =>
                                     handleModalCollaborator(collaborator[0])
                                 }
@@ -41,7 +41,7 @@ const CardCollaborator = ({ user }) => {
                         ) : (
                             <button
                                 type="button"
-                                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 outline-none dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
+                                className="block w-full md:inline-flex md:items-center md:w-initial px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 outline-none dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
                                 onClick={() => addCollaborator(user)}
                             >
                                 Agregar al proyecto
