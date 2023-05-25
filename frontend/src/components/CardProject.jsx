63097f0796e96a1bade62a40aa22058ea43aef8d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-24 20:35:07 -0400

    Fix

--- frontend/src/components/CardProject.jsx
+++ frontend/src/components/CardProject.jsx
@@ -5,7 +5,7 @@ const CardProject = ({ project }) => {
     return (
         <Link
             to={`/projects/${project._id}`}
-            className="block max-w-sm p-6  border bg-gradient-to-r from-cyan-500/70 to-blue-500/20 rounded-lg shadow dark:border-gray-700"
+            className="block md:max-w-sm p-6  border bg-gradient-to-r from-cyan-500/70 to-blue-500/20 rounded-lg shadow dark:border-gray-700"
         >
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">
                 {project.name}
